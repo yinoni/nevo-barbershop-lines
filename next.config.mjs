ef5e490a57e6d43b42de7cbc295982e0a85a8681
yinon2212 <yinonmegidesh2212@gmail.com>
2025-02-27 11:48:15 +0200

    בעיית גרסאות נפתרה אח שלי 🔥

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    webpack: (config) => {
+        config.resolve.fallback = {
+            fs: false,
+            path: false,
+            os: false,
+        };
+        return config;
+    },
+    reactStrictMode: true,
     output: 'export'
 };
 
